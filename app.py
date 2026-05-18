0a6c3b55a1a5685564c4e629eedd71531cc18706
Siddhant Sonawane <siddhantsonawane2004@gmail.com>
2026-05-18 23:53:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -44,5 +44,5 @@ def create_app():
 
 app = create_app()
 
-if __name__ == '__main__':
-    socketio.run(app, debug=True, port=5000)
+if __name__ == "__main__":
+    socketio.run(app, host="0.0.0.0", port=10000)
